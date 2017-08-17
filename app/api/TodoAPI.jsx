1ff8ce38533dd7c9d5bcc104c3aa59327ccac40e
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-17 18:08:26 -0400

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,28 @@ module.exports = {
 
     }
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filtered = todos;
+    filtered = filtered.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    filtered = filtered.filter((todo) => {
+      return searchText.length === 0 || todo.text.toLowerCase().indexOf(searchText) !== -1;
+     });
+
+    filtered.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+
+    return filtered;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -54,4 +54,54 @@ describe('TodoAPI', () => {
       expect(actual).toEqual(todos);
     });
   });
+
+  describe('filterTodos', () =>{
+    var todos = [
+      {
+        id: 1,
+        text: 'Some text here',
+        completed: true
+      },
+      {
+        id: 2,
+        text: 'Other text here',
+        completed: false
+      },
+      {
+        id: 3,
+        text: 'Some more text here',
+        completed: true
+      },
+    ];
+
+    it('should return all items if showCompleted is true', () => {
+      var filterTodos = TodoAPI.filterTodos(todos, true, '');
+
+      expect(filterTodos.length).toBe(3)
+    });
+
+    it('should return non-completed todosx if showCompleted is false', () => {
+      var filterTodos = TodoAPI.filterTodos(todos, false, '');
+
+      expect(filterTodos.length).toBe(1)
+    });
+
+    it('should sort by completed status', () => {
+      var filtered = TodoAPI.filterTodos(todos, true, '')
+
+      expect(filtered[0].completed).toBe(false);
+    });
+
+    it('should return all todos when searchText is empty', () => {
+      var filtered = TodoAPI.filterTodos(todos, true, '')
+
+      expect(filtered[0].completed).toBe(false);
+    });
+
+    it('should return matched todos when searchText is provided', () => {
+      var filtered = TodoAPI.filterTodos(todos, true, 'more')
+
+      expect(filtered.length).toBe(1);
+    });
+  });
 });
