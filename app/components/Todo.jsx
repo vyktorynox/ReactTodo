af5dc955fc68eb6ebd986d70fdd57e4febf33a87
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-18 13:12:09 -0400

    Style Todo application

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,6 +4,7 @@ var moment = require('moment');
 var Todo = React.createClass({
   render: function () {
     var {id, text, completed, createdAt, completedAt} = this.props;
+    var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
       var message = 'Created ';
       var ts = createdAt;
@@ -14,16 +15,18 @@ var Todo = React.createClass({
       return message + moment.unix(ts).format('MMMM Do, YYYY @ hh:mm A');
     };
     return (
-      <div onClick={() => {
-        this.props.onToggle(id);
-      }}>
-      <input type="checkbox" checked={completed}/>
-      <p>{text}</p>
-      <p>{renderDate()}</p>
-
-    </div>
-  )
-}
+      <div className={todoClassName} onClick={() => {
+        this.props.onToggle(id); }}>
+        <div>
+          <input type="checkbox" checked={completed}/>
+        </div>
+        <div>
+          <p>{text}</p>
+          <p className="todo__subtext">{renderDate()}</p>
+        </div>
+      </div>
+    )
+  }
 });
 
 module.exports = Todo;
--- app/components/TodoSearch.jsx
+++ app/components/TodoSearch.jsx
@@ -9,7 +9,7 @@ var TodoSearch = React.createClass({
   },
   render: function () {
     return (
-      <div>
+      <div className="container__header">
         <div>
           <input type="text" ref="searchText" placeholder="Search todos" onChange={this.handleSearch}/>
         </div>
