a9724c3d499f574784c7d3d2dfa838c0e1c4e61f
Luis Javier Ocampo Araoz <vyktorynox@gmail.com>
2017-08-31 17:58:58 -0400

    Wipe todos on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -83,6 +83,21 @@ describe('Reducers', () => {
       expect(res.length).toEqual(1);
       expect(res[0]).toEqual(todos[0]);
     });
+    it('should wipe todos on LOGOUT', () => {
+      var todos = [{
+        id: '111',
+        text: 'Anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 33000
+      }];
+      var action = {
+        type: 'LOGOUT'
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res.length).toEqual(0);
+    });
   });
   describe('authReducer', () => {
     it('should store uid on LOGIN', () => {
